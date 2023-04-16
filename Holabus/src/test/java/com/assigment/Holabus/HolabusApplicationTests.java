0cdd5ed1647c5c49886b891832085177295cb126
Hoang Quoc Anh <102196398+akaisaiza@users.noreply.github.com>
2023-04-16 22:19:46 +0700

    start

---
+++ Holabus/src/test/java/com/assigment/Holabus/HolabusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment.Holabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolabusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
