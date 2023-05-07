d5d4d5b551fd5d724c4f19a0de1b6509eabf00fe
Hoang Quoc Anh <102196398+akaisaiza@users.noreply.github.com>
2023-05-07 16:09:10 +0700

    Add get info

--- Holabus/src/main/java/com/assigment/Holabus/Controller/UserController.java
+++ Holabus/src/main/java/com/assigment/Holabus/Controller/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,9 @@ public class UserController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+    @PatchMapping("/info")
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
     }
 }
